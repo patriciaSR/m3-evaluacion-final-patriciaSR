f38b094c1a4869cc8ec40d87c50b2f11b6156fbf
patriciaSR <patricia.suarez.psr@gmail.com>
2019-09-23 11:41:02 +0200

    fix api url

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.scss';
 
 const Header = () => {
   return (
     <header className="page__header">
-      <h1 className="page__title">Directorio de personajes de Rick y Morty</h1>
+      <div className="title__container">
+        <h1 className="page__title">Directorio de personajes de Rick y Morty</h1>
+      </div>
     </header>
   )
 };
--- src/services/getCharacters.js
+++ src/services/getCharacters.js
@@ -1,4 +1,6 @@
-const ENDPOINT = 'https://rickandmortyapi.com/api/character/';
+const ENDPOINT = 'https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json';
+
+
 
 const getCharacters = () => {
   return fetch(ENDPOINT).then(response => response.json())
