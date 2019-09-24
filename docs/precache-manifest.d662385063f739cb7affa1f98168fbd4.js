d1c11f8cd66e74f27d938673b2cc338c7690e189
patriciaSR <patricia.suarez.psr@gmail.com>
2019-09-24 10:41:18 +0200

    update docs folder

---
+++ docs/precache-manifest.d662385063f739cb7affa1f98168fbd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6903ae53531bcc745423120ed3d869d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1809d179cd483ed27ce0",
+    "url": "./static/css/main.41768334.chunk.css"
+  },
+  {
+    "revision": "78b79c3731458ebe8729",
+    "url": "./static/js/2.1a6c6ea0.chunk.js"
+  },
+  {
+    "revision": "1809d179cd483ed27ce0",
+    "url": "./static/js/main.30b9f461.chunk.js"
+  },
+  {
+    "revision": "1d826c391be1de5f3809",
+    "url": "./static/js/runtime-main.06f57862.js"
+  },
+  {
+    "revision": "a68be4a16abb30ae947a262e4ca07192",
+    "url": "./static/media/mrs-mee.a68be4a1.gif"
+  },
+  {
+    "revision": "079e83c05b102592493c891bc7893bd5",
+    "url": "./static/media/no-results.079e83c0.gif"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/rick-logo.75509f2e.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce388184a8c70ca22d818a7c40e68781.js"
+  "./precache-manifest.d662385063f739cb7affa1f98168fbd4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
